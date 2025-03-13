26b6309c79afae1f7c8df04920373b9a2ae74ec8
Caio Pinheiro <86423541+caiofpinheiro@users.noreply.github.com>
2025-03-13 11:15:59 +0000

    springboot

--- atividade/src/main/java/com/senai/atividade/AtividadeApplication.java
+++ atividade/src/main/java/com/senai/atividade/AtividadeApplication.java
@@ -2,6 +2,7 @@ package com.senai.atividade;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class AtividadeApplication {
@@ -9,5 +10,8 @@ public class AtividadeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AtividadeApplication.class, args);
 	}
-
+@RequestMapping("/")
+	public String atividade() {
+		return "desgraca games";
+	}
 }
